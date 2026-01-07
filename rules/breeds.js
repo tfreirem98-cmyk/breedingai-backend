033bf3eadaaf37d01cbde5bcafdae2c936fdb88a
tfreirem98-cmyk <tfreirem98@gmail.com>
2026-01-07 15:04:24 +0100

    Update breeds.js

--- rules/breeds.js
+++ rules/breeds.js
@@ -1,62 +1,26 @@
-<select id="raza">
-  <option value="">Selecciona una raza</option>
-
-  <!-- Razas de trabajo -->
-  <option>Pastor Alemán</option>
-  <option>Border Collie</option>
-  <option>Pastor Belga Malinois</option>
-  <option>Pastor Belga Tervueren</option>
-  <option>Pastor Australiano</option>
-  <option>Rottweiler</option>
-  <option>Doberman</option>
-  <option>Husky Siberiano</option>
-  <option>Akita Inu</option>
-  <option>Alaskan Malamute</option>
-
-  <!-- Razas de caza -->
-  <option>Labrador Retriever</option>
-  <option>Golden Retriever</option>
-  <option>Pointer Inglés</option>
-  <option>Setter Inglés</option>
-  <option>Setter Irlandés</option>
-  <option>Braco Alemán</option>
-  <option>Weimaraner</option>
-  <option>Cocker Spaniel Inglés</option>
-  <option>Springer Spaniel</option>
-  <option>Teckel (Dachshund)</option>
-
-  <!-- Razas molosoides -->
-  <option>Bulldog Inglés</option>
-  <option>Bulldog Francés</option>
-  <option>Bóxer</option>
-  <option>Mastín Español</option>
-  <option>Dogo Argentino</option>
-  <option>Cane Corso</option>
-  <option>Fila Brasileiro</option>
-  <option>Gran Danés</option>
-
-  <!-- Razas de compañía -->
-  <option>Caniche (Toy / Mini / Estándar)</option>
-  <option>Bichón Maltés</option>
-  <option>Shih Tzu</option>
-  <option>Pomerania</option>
-  <option>Chihuahua</option>
-  <option>Yorkshire Terrier</option>
-  <option>Boston Terrier</option>
-  <option>Pug</option>
-
-  <!-- Razas primitivas -->
-  <option>Basenji</option>
-  <option>Shiba Inu</option>
-  <option>Samoyedo</option>
-  <option>Chow Chow</option>
-
-  <!-- Otras comunes en cría -->
-  <option>American Staffordshire Terrier</option>
-  <option>Staffordshire Bull Terrier</option>
-  <option>Jack Russell Terrier</option>
-  <option>Beagle</option>
-  <option>Galgo Español</option>
-  <option>Whippet</option>
-  <option>Podenco Ibicenco</option>
-</select>
+export const breeds = [
+  "Akita Inu",
+  "Alaskan Malamute",
+  "Australian Shepherd",
+  "Beagle",
+  "Border Collie",
+  "Boxer",
+  "Bulldog Francés",
+  "Bulldog Inglés",
+  "Cane Corso",
+  "Chihuahua",
+  "Cocker Spaniel",
+  "Doberman",
+  "Golden Retriever",
+  "Labrador Retriever",
+  "Mastín Español",
+  "Pastor Alemán",
+  "Pastor Belga Malinois",
+  "Pomerania",
+  "Rottweiler",
+  "Samoyedo",
+  "Shiba Inu",
+  "Siberian Husky",
+  "Teckel",
+  "Yorkshire Terrier"
+];
